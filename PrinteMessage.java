1a52a45f85375486d88dab8be0b1d7283296b075
PathfinderKyra <pathfinderkyra@gmail,com>
2024-02-20 22:18:27 -0700

    Reformat Commit

--- PrinteMessage.java
+++ PrinteMessage.java
@@ -1,5 +1,5 @@
 public class PrinteMessage {
-        public static void main (String[] args) {
-            System.out.println("This is my first programming test");
-        }
+    public static void main(String[] args) {
+        System.out.println("This is my first programming test");
+    }
 }
